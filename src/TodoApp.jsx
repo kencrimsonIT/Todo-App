cd66c2b70079ddc0f7d1906bd236638161f9f4f3
kencrimsonIT <tongkien930@gmail.com>
2025-08-16 22:44:06 +0700

    import header component

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './CSS/TodoApp.css';
 
+import Header from './Components/Header'
+
 function TodoApp() {
     return (
         <div className='container'>
-            
+            <Header />
         </div>
     );
 }
