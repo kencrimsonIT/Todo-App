35e3ecde0296a1cb0080e7353f67e98d93d41079
kencrimsonIT <tongkien930@gmail.com>
2025-08-16 22:38:07 +0700

    Add header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import '../CSS/Header.css';
 
 function Header() {
-
+    return (
+        <div className='header-container'>
+            <h1 className='header-title'>📝 Todo App</h1>
+            <p className='header-subtitle'>Make your day easier 👍</p>
+        </div>
+    );
 }
 
 export default Header;
