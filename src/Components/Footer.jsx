9d0176d2d2533de407a5c2fe735b7574983a5e48
kencrimsonIT <tongkien930@gmail.com>
2025-08-16 23:34:10 +0700

    Add footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import '../CSS/Footer.css'
 
 function Footer() {
-
+    return (
+        <div className='footer-container'>
+            <p className='footer-text'>
+                Press <strong>Enter</strong> to add your tasks • Click the tasks to edit
+            </p>
+        </div>
+    );
 }
 
 export default Footer;
