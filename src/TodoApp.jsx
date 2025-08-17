e45a5cd6a84bbe34525d386bc2c7765b51451bc0
kencrimsonIT <tongkien930@gmail.com>
2025-08-17 23:47:59 +0700

    import task adding component & useTodo hook

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import './TodoApp.css'
 
-import Header from './Components/Header'
+import Header from './Components/Header';
+import AddTask from "./Components/AddTask";
 import Footer from "./Components/Footer";
 
+import {useTodo} from './Hooks/useTodo.js';
+
 function TodoApp() {
+    const {
+        todos,
+        editingId,
+        editingText,
+        setEditingText,
+        addTodo,
+        deleteTodo,
+        toggleTodo,
+        startEditing,
+        saveEdit,
+        cancelEdit
+    } = useTodo();
+
     return (
         <div className='container'>
             <Header />
+            <AddTask onAddTodo={addTodo} />
             <Footer />
         </div>
     );
