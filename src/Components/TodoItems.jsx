ed8b82bf47111872d1c645c3b1456a8e36c9aaaf
kencrimsonIT <tongkien930@gmail.com>
2025-08-17 23:44:50 +0700

    Create todo task items

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,7 +1,74 @@
 import React from "react";
+import {Check} from 'lucide-react';
+import TodoAction from '../Components/TodoAction';
+import '../CSS/TodoItems.css';
 
-function TodoItems() {
+function TodoItems(
+    {
+        todo,
+        onToggle,
+        onDelete,
+        onStartEdit,
+        onSaveEdit,
+        onCancelEdit,
+        editingId,
+        editingText,
+        setEditingText
+    }
+) {
+    const isEditing = editingId === todo.id;
 
+    const handleEditKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            onSaveEdit();
+        } else if (e.key === 'Escape') {
+            onCancelEdit();
+        }
+    }
+
+    return (
+        <li className='todo-tasks-container'>
+            <div className='check-button'>
+                <button
+                    onClick={() => onToggle(todo.id)}
+                    className={`todo-toggle-button ${todo.completed ? 'completed' : 'incompleted'}`}
+                >
+                    {todo.completed && <Check className='check-icon' />}
+                </button>
+
+                <div className='editing-section'>
+                    {isEditing ? (
+                        <input
+                            type="text"
+                            value={editingText}
+                            onChange={(e) => setEditingText(e.target.value)}
+                            onKeyPress={handleEditKeyPress}
+                            onBlur={onSaveEdit}
+                            autoFocus
+                            className='todo-input'
+                        />
+                    ) : (
+                        <div>
+                            <p className={`todo-text ${todo.completed ? 'completed' : ''}`}>
+                                {todo.text}
+                            </p>
+                            <p className='todo-date'>
+                                {new Date(todo.createdAt).toLocaleDateString('vi-VN')}
+                            </p>
+                        </div>
+                    )}
+                </div>
+
+                <TodoAction
+                    isEditing={isEditing}
+                    onStartEdit={() => onStartEdit(todo.id, todo.text)}
+                    onSaveEdit={onSaveEdit}
+                    onCancelEdit={onCancelEdit}
+                    onDelete={() => onDelete(todo.id)}
+                />
+            </div>
+        </li>
+    );
 }
 
 export default TodoItems;
