207e169da96b21a0bbd62952aa3ea6a05b796f40
kencrimsonIT <tongkien930@gmail.com>
2025-08-18 15:53:42 +0700

    Create todo list component

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import TodoItems from "../Components/TodoItems.jsx";
+import '../CSS/TodoList.css'
 
-function TodoList() {
-
+function TodoList(
+    {
+        todos,
+        onToggle,
+        onDelete,
+        onStartEdit,
+        onSaveEdit,
+        onCancelEdit,
+        editingId,
+        editingText,
+        setEditingText
+    }
+) {
+    return (
+        <div className='todo-list-container'>
+            <ul className='todo-list'>
+                {todos.map(function(todo){
+                    return (
+                        <TodoItems
+                            key={todo.id}
+                            todo={todo}
+                            onToggle={onToggle}
+                            onDelete={onDelete}
+                            onStartEdit={onStartEdit}
+                            onSaveEdit={onSaveEdit}
+                            onCancelEdit={onCancelEdit}
+                            editingId={editingId}
+                            editingText={editingText}
+                            setEditingText={setEditingText}
+                        />
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default TodoList;
